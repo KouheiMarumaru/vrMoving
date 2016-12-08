d6306a96556f5536684617dafb3c3392757cafb3
KouheiMatumaru <matumarukouhei@gmail.com>
2016-12-08 16:52:24 +0900

    ソース追加

---
+++ vrMoving/Assets/Script/Coroutine.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Coroutine : MonoBehaviour {
+
+	public int f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("MoveSphere");
+	}
+
+
+	// Update is called once per frame
+	IEnumerator MoveSphere(){
+
+			this.gameObject.transform.position = new Vector3(f*0.1f, 1, 1); 
+			yield return new WaitForSeconds(0.01f);
+
+	}
+}
---
+++ vrMoving/Assets/Script/RayManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RayManager : MonoBehaviour {
+
+	public GameObject dive_Camera;
+	public Image reticle;
+	public int f;
+//	RaycastHit hit;
+
+	public GameObject player;
+
+	//rayで示した地点
+	Vector3 newPlayerPosition;
+
+	//オブジェクトが到達するまでの時間
+	float time = 0;
+
+
+
+	//	public GameObject alignment;
+
+//	void Start () {
+//		StartCoroutine("MoveSphere");
+//	}
+
+	void Update () {
+//		time = 0;
+		//ある起点から、特定の方向に向けて出す光線を生成
+		//起点がdive_Cameraの位置、方向がdive_Cameraから見て前方向に向けて光線
+		Ray ray = new Ray (dive_Camera.transform.position, dive_Camera.transform.forward);
+
+		RaycastHit hit = new RaycastHit ();
+//		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit)) {
+			//hitした場所にImageであるalignmentが表示される
+//			reticle.rectTransform.position = hit.point;
+//			alignment.transform.position = hit.point;
+			newPlayerPosition = hit.point;
+			Debug.DrawLine (dive_Camera.transform.position, newPlayerPosition, Color.black);
+			print (newPlayerPosition);
+
+//			StartCoroutine("MoveSphere");
+
+			time = Time.deltaTime * 1f;
+			player.transform.position = Vector3.Lerp (player.transform.position, newPlayerPosition, time);
+		}
+	}
+
+
+//			IEnumerator MoveSphere(){
+//
+//		this.gameObject.transform.position = hit;
+//
+//		yield return new WaitForSeconds(0.01f);
+//
+//	}
+}
